6f9374165b48c52544224f18bb8025953784ab29
Kasiet Dokturbaeva <dk14006@auca.kg>
2026-05-16 15:45:55 +0600

    added file and format tools

--- main_window.cpp
+++ main_window.cpp
@@ -2,6 +2,9 @@
 
 #include <QTextEdit>
 #include <QMenuBar>
+#include <QApplication>
+#include <QFileDialog>
+#include <QMessageBox>
 
 main_window::main_window()
 {
@@ -15,9 +18,112 @@ main_window::main_window()
     transforms.push_back(std::make_unique<capitalize_transform>());
     transforms.push_back(std::make_unique<sentence_case_transform>());
     transforms.push_back(std::make_unique<swap_case_transform>());
+
+    setup_file_menu();
+    setup_format_menu();
 }
 
 main_window::~main_window() = default;
+
+void main_window::setup_file_menu()
+{
+    auto* file_menu = menuBar()->addMenu("File");
+
+    const auto* action_new = file_menu->addAction("New");
+    connect(action_new, &QAction::triggered, this, [this]
+    {
+        editor->clear();
+        current_file.clear();
+        update_title();
+    });
+
+    file_menu->addSeparator();
+
+    const auto* action_open = file_menu->addAction("Open...");
+    connect(action_open, &QAction::triggered, this, [this]
+    {
+        open_file();
+    });
+
+    const auto* action_save = file_menu->addAction("Save");
+    connect(action_save, &QAction::triggered, this, [this]
+    {
+        save_file();
+    });
+
+    const auto* action_save_as = file_menu->addAction("Save As...");
+    connect(action_save_as, &QAction::triggered, this, [this]
+    {
+        save_file_as();
+    });
+
+    file_menu->addSeparator();
+
+    const auto* action_exit = file_menu->addAction("Exit");
+    connect(action_exit, &QAction::triggered, this, []
+    {
+        QApplication::quit();
+    });
+}
+
+void main_window::open_file()
+{
+    const auto path = QFileDialog::getOpenFileName(this, "Open File");
+    if (path.isEmpty())
+    {
+        return;
+    }
+    QFile file(path);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        return;
+    }
+    QTextStream in(&file);
+    editor->setPlainText(in.readAll());
+    current_file = path;
+    update_title();
+}
+
+void main_window::save_file()
+{
+    if (current_file.isEmpty())
+    {
+        save_file_as();
+        return;
+    }
+    QFile file(current_file);
+    if (!file.open(QIODevice::WriteOnly | QIODevice::Text))
+    {
+        return;
+    }
+    QTextStream out(&file);
+    out << editor->toPlainText();
+}
+
+void main_window::save_file_as()
+{
+    const auto path = QFileDialog::getSaveFileName(this, "Save File As");
+    if (path.isEmpty())
+    {
+        return;
+    }
+    current_file = path;
+    save_file();
+    update_title();
+}
+
+void main_window::update_title()
+{
+    if (current_file.isEmpty())
+    {
+        setWindowTitle("Notepad");
+    }
+    else
+    {
+        setWindowTitle("Notepad: " + current_file);
+    }
+}
+
 
 void main_window::setup_format_menu()
 {
--- main_window.h
+++ main_window.h
@@ -6,6 +6,8 @@
 #include "text_transforms.h"
 #include <QMainWindow>
 #include <QTextEdit>
+#include <memory>
+#include <vector>
 
 class main_window : public QMainWindow {
     Q_OBJECT
@@ -15,10 +17,17 @@ public:
     ~main_window() override;
 
 private:
+    void setup_file_menu();
     void setup_format_menu();
     void apply_transform(const text_transform& transform) const;
 
+    void open_file();
+    void save_file();
+    void save_file_as();
+    void update_title();
+
     QTextEdit* editor = nullptr;
+    QString current_file;
     std::vector<std::unique_ptr<text_transform>>transforms;
 
 };
